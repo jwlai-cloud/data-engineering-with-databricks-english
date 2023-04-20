bfb42a1f341f37a1129d35f60066e605f0e99869
Sourcery AI <>
2023-04-20 02:52:45 +0000

    'Refactored by Sourcery'

--- Solutions/Includes/Classroom-Setup-03.2B.py
+++ Solutions/Includes/Classroom-Setup-03.2B.py
@@ -13,13 +13,15 @@ if dbgems.is_job():
     print("Mocking global temp view")
     spark.sql(f"""USE {DA.schema_name}""")
     spark.sql(f"""CREATE TABLE external_table USING CSV OPTIONS (path='{DA.paths.datasets}/flights/departuredelays.csv', header="true", mode="FAILFAST");""")
-    spark.sql(f"""CREATE OR REPLACE GLOBAL TEMPORARY VIEW global_temp_view_dist_gt_1000 AS SELECT * FROM external_table WHERE distance > 1000;""")
-    
+    spark.sql(
+        """CREATE OR REPLACE GLOBAL TEMPORARY VIEW global_temp_view_dist_gt_1000 AS SELECT * FROM external_table WHERE distance > 1000;"""
+    )
+
 else:
     lesson_config.create_schema = False
     DA = DBAcademyHelper(course_config, lesson_config)
     DA.init()
-    
+
 print()
 
 DA.conclude_setup()
